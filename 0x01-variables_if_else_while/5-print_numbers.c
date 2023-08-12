7f3b9b9973be83a9dd298f7cfe5f0510a229b9f4
NegrosG <negrosmartins@gmail.com>
2023-08-11 20:30:20 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function contains all codes
+ *
+ * Return: (0) is a success
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 0 ; j < 10 ; j++)
+	printf("%d", j);
+	printf("\n");
+	return (0);
+
+}
