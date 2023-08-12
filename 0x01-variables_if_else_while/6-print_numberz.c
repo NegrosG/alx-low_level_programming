c056700240e0f17534a87ad83346a947c57c95c4
NegrosG <negrosmartins@gmail.com>
2023-08-11 20:54:02 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main functions
+ *
+ * Return: (0) is a success
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 0 ; j < 10 ; j++)
+		putchar(j + '0');
+			putchar('\n');
+			return (0);
+}
