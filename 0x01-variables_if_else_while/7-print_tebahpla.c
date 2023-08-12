4e797c5d390ba313e3f681d1841d2f05714d6e09
NegrosG <negrosmartins@gmail.com>
2023-08-11 21:03:18 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - all codes are in main function
+ *
+ * Return: (0) is regarded as a success
+ */
+
+int main(void)
+{
+	char k;
+
+	for (k = 'z' ; k >= 'a' ;  k--)
+		putchar(k);
+	putchar('\n');
+		return (0);
+}
