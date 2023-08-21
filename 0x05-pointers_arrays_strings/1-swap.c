50a9231f338f0cfd36f016d53e33e351cab16e8f
NegrosG <negrosmartins@gmail.com>
2023-08-21 15:43:36 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - this functions to swap pointers
+ * @a: the first value
+ * @b: the second nd value
+ * Retur: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+	
+	c = *a;
+	*a = *b;
+	*b = c;
+}
