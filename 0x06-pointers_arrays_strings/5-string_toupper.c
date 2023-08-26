37614266b59012be7d14f436d0e9d2561578f070
NegrosG <negrosmartins@gmail.com>
2023-08-25 17:02:04 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - this function chnages all lower case in string to uppercase
+ * @x: pointer to string
+ * Return: uppercase string
+ */
+
+
+char *string_toupper(char *x)
+{
+	int lenght;
+
+	lenght = 0;
+	while (x[lenght] != '\0')
+	{
+		if (x[lenght] >= 97 && x[lenght] <= 122)
+		{
+			x[lenght] = x[lenght] - 32;
+		}
+		lenght++;
+	}
+	return (x);
+}
