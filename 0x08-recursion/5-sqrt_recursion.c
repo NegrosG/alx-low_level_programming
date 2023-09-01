680e8b7e107074f94b6e03c3ef38c53d967b4816
NegrosG <negrosmartins@gmail.com>
2023-08-31 17:46:30 -0700

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,33 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-  *_sqrt_recursion - this function prints a square root of a number
-  *@n: we want to 
+  *_sqrt_recursion - this function return the natural square root of a number
+  *@n: int number
+  *Return: no square root, return -1. else return natural
+  */
+
+int _sqrt_recursion(int n)
+{
+	return (mar(n, 1));
+}
+
+/**
+  *mar - a function to solve the recursion
+  *@j: number to determine if square root
+  *@i: incremenr int to compare j
+  *Return: square root if natural square root or -1 if none found
+  */
+
+int mar(int i, int j)
+{
+	int square;
+
+	square = j * j;
+	if (square == i)
+		return (j);
+	else if (square < i)
+		return (mar(i, j + 1));
+	else
+		return (-1);
+}
+
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
+int mar(int i,int j);
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
