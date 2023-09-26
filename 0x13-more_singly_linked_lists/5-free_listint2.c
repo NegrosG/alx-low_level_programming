9aa0d483cb58aeb4f80099c32fd1315a440ea2b5
NegrosG <negrosmartins@gmail.com>
2023-09-25 17:21:30 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *freenode;
 
-	if (head == NULL)
-		return;
-	if (*head == NULL)
-		return;
+	if (head == NULL || *head == NULL)
 
 	while (*head != NULL)
 	{
